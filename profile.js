c788565552d1d773d2afd2282bc40945bfd676c6
omarr23 <omar.salah2015.os@gmail.com>
2025-07-17 16:06:39 +0300

    fixed

--- profile.js
+++ profile.js
@@ -12,7 +12,7 @@
         // Ensure token is available, otherwise redirect to login
         var token = localStorage.getItem('token');
         if (!token) {
-            window.location.href = window.location.origin + '/login.html';
+            window.location.href = "https://abdelrhmanaymanfathi.github.io/Deployment-Gamaia/login.html";
         }
 
         // Load profile data
@@ -64,7 +64,7 @@
         function logout() {
             localStorage.removeItem('token');
             localStorage.removeItem('user');
-            window.location.href = window.location.origin + '/login.html';
+            window.location.href = "https://abdelrhmanaymanfathi.github.io/Deployment-Gamaia/login.html";
         }
 
         // =====================
@@ -244,7 +244,7 @@
         document.getElementById("logout-btn").addEventListener("click", () => {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
-  window.location.href = "https://abdelrhmanaymanfathi.github.io/login.html";
+  window.location.href = "https://abdelrhmanaymanfathi.github.io/Deployment-Gamaia/login.html";
             });
 
             // Toggle profile dropdown menu
